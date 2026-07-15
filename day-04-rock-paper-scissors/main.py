ab05b907771cbaa951f95e6e6b7042ba566b0d46
Your Name <146131111+zain-ahm05@users.noreply.github.com>
2026-07-15 23:24:53 +0530

    Complete Day 04: Rock Paper Scissors

---
+++ day-04-rock-paper-scissors/main.py
@@ -0,0 +1,50 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+game_images = [rock, paper, scissors]
+user_choice = int(input("What do you like to choose? Type 0 for Rock, 1 for paper and 2 for scissors: "))
+print(f"You chose {user_choice}")
+if user_choice >= 0 and user_choice <= 2:
+    print(game_images[user_choice])
+
+computer_choice = random.randint(0, 2)
+print("Computer chose:")
+print(game_images[computer_choice])
+
+if user_choice >=3 or user_choice < 0:
+    print("You typed an invalid number")
+elif user_choice == 0 and  computer_choice == 2:
+    print("You win")
+elif computer_choice == 0 and user_choice==0:
+    print("You lose")
+elif computer_choice > user_choice:
+    print("You lose")
+elif user_choice > computer_choice:
+    print("You win!")
+elif user_choice == computer_choice:
+    print("Its a draw")
